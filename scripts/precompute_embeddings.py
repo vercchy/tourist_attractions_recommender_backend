86d9b29df326cdb3613542084f7f43335a57b457
vercchy <verchepetrushevska2@gmail.com>
2024-12-08 21:10:14 +0100

    Add ml-models and a script for calculating attractions embeddings and storing them in redis cache

---
+++ scripts/precompute_embeddings.py
@@ -0,0 +1,53 @@
+import pickle
+import redis
+from app.models.tourist_attraction import TouristAttraction
+from app.service.embedding import load_glove_model, compute_embedding
+from app.db.session import get_db
+from sqlalchemy.orm import Session
+from app.utils.redis import get_redis_client
+
+EMBEDDINGS_FILE = "embeddings.pkl"
+
+
+def save_embeddings_to_file(database: Session, model, file_path=EMBEDDINGS_FILE):
+    embeddings = {}
+
+    attractions = database.query(TouristAttraction).all()
+    for attraction in attractions:
+        combined_text = (
+            f"{attraction.description or ''} {attraction.comment or ''}".strip()
+            if (attraction.description or '') != (attraction.comment or '')
+            else attraction.description or ''
+        )
+        embedding = compute_embedding(combined_text, model)
+        embeddings[attraction.id] = embedding
+
+    with open(file_path, "wb") as f:
+        pickle.dump(embeddings, f)
+
+    print(f"Embeddings saved locally to file {file_path}")
+
+
+def load_embeddings_from_file(file_path=EMBEDDINGS_FILE):
+    with open(file_path, "rb") as f:
+        embeddings = pickle.load(f)
+    print(f"Loaded embeddings from file {file_path}")
+    return embeddings
+
+
+def populate_reddis_from_file(redis_client, file_path=EMBEDDINGS_FILE):
+    embeddings = load_embeddings_from_file(file_path)
+    for attraction_id, embedding in embeddings.items():
+        redis_client.set(f"attraction:{attraction_id}", embedding.tobytes())
+        print(f"Added attraction with id {id} to redis cache")
+
+    print("Redis cache populated from file.")
+
+
+if __name__ == "__main__":
+    db = next(get_db())
+    glove_model = load_glove_model()
+    save_embeddings_to_file(db, glove_model)
+    populate_reddis_from_file(get_redis_client())
+
+
