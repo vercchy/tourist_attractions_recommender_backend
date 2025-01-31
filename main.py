8edba93a96d1045e815e8d703e11d120cc1d021c
Rusev479 <arusev479@gmail.com>
2025-01-31 17:57:08 +0100

    Removed unneeded things from main

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import importlib
 from app.db.session import get_db
 from app.utils.redis import get_redis_client
 from app.service.embedding import load_glove_model
-from app.service.recommendations.utils import InteractionsMatrixHelper
-from app.service.collaborative_filtering.collaborative_model import train_and_cache_collaborative_model
+
+
 
 router_modules = [
     "app.api.authentication",
@@ -27,13 +27,6 @@ async def load_glove_model_on_startup():
 
     db = next(get_db())
     redis_client = await get_redis_client()
-
-    interactions_matrix_helper = InteractionsMatrixHelper(db, redis_client)
-    await interactions_matrix_helper.create_user_interactions_matrix()
-    print("User interactions matrix created")
-
-    await train_and_cache_collaborative_model(db, redis_client)
-    print("Collaborative filtering user-attraction predictions created for the first time")
 
 
 @app.on_event("shutdown")
